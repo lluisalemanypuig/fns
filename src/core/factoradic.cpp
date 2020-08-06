a82bddc4f643fe0f3f6f79cae228dd0d28d3a9bc
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-08-06 09:12:04 +0200

    Updated license headers

--- src/core/factoradic.cpp
+++ src/core/factoradic.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************
  * Factorial Number System
- * Copyright (C) 2018  Lluís Alemany Puig
+ * Copyright (C) 2018-2020  Lluís Alemany Puig
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
